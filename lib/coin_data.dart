1160c01aa8e8743fd0be68187344422ef874cdb1
Mohammad Salim <mhmadip@gmail.com>
2021-03-12 20:12:39 +0300

    Update coin_data.dart

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -6,7 +6,7 @@ class CurrencyRate {
 
   Future<dynamic> getData(String coin, String base) async {
     // http://example.org/path?q=dart.
-    //"https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=93811603-9BE1-4976-94B8-0000000000";
+    //"https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=YOUR-API-KEY-HERE";
     Uri url = Uri.https("rest.coinapi.io", "/v1/exchangerate/$base/$coin",
         {"apikey": "YOUR-API-KEY-HERE"});
     //Await the http get response, then decode the json-formatted response.
